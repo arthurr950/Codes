f41fd56df658b3b2fb9a6c8a473493a8e807a6f1
Arthur Gava <artgs1234@hotmail.com>
2023-07-24 16:48:04 -0300

    Exercicio 11 - Enum e composicao

---
+++ Exercicio 11/Exercicio 11/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_11.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment,
+        Processing,
+        Shipped,
+        Delivered
+    }
+}
---
+++ Exercicio 11/Exercicio 11/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Exercicio_11.Entities;
+using Exercicio_11.Entities.Enums;
+using System.Globalization;
+
+namespace Exercicio_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime bd = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, bd);
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = (OrderStatus)Enum.Parse(typeof(OrderStatus), Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Enter #" +i+ " item data: ");
+                Console.Write("Product name: ");
+                string namep= Console.ReadLine();
+                Console.Write("Product price: ");
+                double pricep = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantityp = int.Parse(Console.ReadLine());
+
+                Product product = new Product(namep, pricep);
+                OrdemItem ordemItem = new OrdemItem(quantityp, pricep, product);
+
+                order.AddItem(ordemItem);
+            }
+
+            
+
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.Write(order);
+
+
+
+
+        }
+    }
+}
